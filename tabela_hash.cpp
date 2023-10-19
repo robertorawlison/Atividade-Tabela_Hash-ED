22b8271db0d52542bca48d8a06f5695a6bf10d73
robertorawlison <130768898+robertorawlison@users.noreply.github.com>
2023-10-19 19:33:36 -0300

    Update tabela_hash

--- tabela_hash.cpp
+++ tabela_hash.cpp
@@ -4,49 +4,63 @@
 
 using namespace std;
 
-class Tabela_Hash {
-private:
-    static const int tamanho_primeiro_nivel = 10;
-    int tamanho_segundo_nivel;
-    vector<vector<list<int>>> primeiro_nivel;
+//tabela hash duplamente encandeada
+
+class tabela_h {
+    static const int tamanho_level1 = 10;
+    int tamanho_n;
+    vector<vector<list<int>>> tabela;
 public:
-    Tabela_Hash(int tamanho) {
-        tamanho_segundo_nivel = (tamanho * 1.25) / 10;
-        primeiro_nivel.resize(tamanho_primeiro_nivel);
+    tabela_h(int t){
+    tamanho_n = t/10;
+    tabela.resize(tamanho_level1);
+    for (int i = 0; i < tamanho_level1; ++i) {
+        tabela[i].resize(tamanho_n);
     }
-    // Função para inserir um valor na tabela hash
+}
 
     void inserir(int valor){
-        int idx_nivel1 =  valor/tamanho_primeiro_nivel;
-        int idx_nivel2 = valor/tamanho_segundo_nivel;
+        int idx1 = valor % tamanho_level1;
+        int idx2 = valor % tamanho_n;
 
-        primeiro_nivel[idx_nivel1][idx_nivel2].push_back(valor);
+        tabela[idx1][idx2].push_back(valor);
     }
-    // Função para remover um valor da tabela hash
+
     void remover(int valor){
-        int idx_nivel1 = valor % tamanho_primeiro_nivel;
-        int idx_nivel2 = valor % tamanho_segundo_nivel;
+        int idx1 = valor % tamanho_level1;
+        int idx2 = valor % tamanho_n;
 
-        primeiro_nivel[idx_nivel1][idx_nivel2].remove(valor);
+        tabela[idx1][idx2].remove(valor);
     }
 
-    void imprimir() {
-        for (int i = 0; i < tamanho_primeiro_nivel; i++) {
-            for (int j = 0; j < tamanho_segundo_nivel; j++) {
-                std::cout << "Primeiro Nível: " << i << " Segundo Nível: " << j << " -> ";
+    bool buscar(int valor){
+        int idx1 = valor % tamanho_level1;
+        int idx2 = valor % tamanho_n;
 
-                for (const auto& valor : primeiro_nivel[i][j]) {
-                    std::cout << valor << " ";
-                }
-                std::cout << std::endl;
+        for(const auto &val : tabela[idx1][idx2]){
+            if(val == valor){
+                return true;
             }
         }
+        return false;
     }
 
+    void imprimir(){
+        for(int i = 0; i < tamanho_level1; i++){
+            for(int j = 0; j < tamanho_n; j++){
+                cout << "Posicao [" << i << "][" << j << "]: ";
+                for(const auto &val : tabela[i][j]){
+                    cout << val << " ";
+                }
+                cout << endl;
+            }
+        }
+        cout << endl;
+    }
 };
 
 int main(){
-    Tabela_Hash tabela(100);
+    tabela_h tabela(20);
 
     tabela.inserir(5);
     tabela.inserir(15);
@@ -54,11 +68,16 @@ int main(){
     tabela.inserir(7);
     tabela.inserir(17);
 
+    cout << "Buscando valor: 15" << endl;
+    cout << "resultado busca: " << tabela.buscar(15) << endl;
+
+    cout << "Tabela pos insercao: " << endl;
     tabela.imprimir();
 
     tabela.remover(25);
     tabela.remover(17);
 
+    cout << "Tabela pos remocao: " << endl;
     tabela.imprimir();
 
     return 0;
